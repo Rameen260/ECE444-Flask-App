1dad62580987c41b0a5026c9bb3397d1e5123edc
Rameen260 <98580766+Rameen260@users.noreply.github.com>
2024-10-01 19:09:17 -0400

    Ran Database
    ran db.create_all(), test cases still passing

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=R:\school\444\PRA3\flaskr-tdd\env
+set VIRTUAL_ENV=R:\school\444\ECE444-Flask-App\env
 
 if not defined PROMPT set PROMPT=$P$G
 
